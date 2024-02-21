9c08cbf29797a77939bea14f99243da11241009c
unAbel <unrealabel@gmail.com>
2024-02-21 12:02:15 -0500

    partially implement main view

---
+++ CryptoCurrency/App/CryptoCurrencyApp.swift
@@ -0,0 +1,19 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by macOS abel on 21/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CryptoCurrencyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ CryptoCurrency/Core/Home/Views/TopMoverView.swift
@@ -0,0 +1,18 @@
+//
+//  TopMoverView.swift
+//  CryptoCurrency
+//
+//  Created by macOS abel on 21/02/24.
+//
+
+import SwiftUI
+
+struct TopMoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopMoverView()
+}
